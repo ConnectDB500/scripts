659c83eb3d457f354552dad8c28131476927ffb0
Connect <liberalinoenterprise500@gmail.com>
2025-08-06 00:06:21 -0300

    update

---
+++ aula04.sh
@@ -0,0 +1,45 @@
+Firewall, Python HTTP Server e Gerenciamento de Portas - Resumo
+
+Para adicionar repositarios ao nosso sistema, precisamos de um software chamado software-properties-common, que pode ser instalado com o comando abaixo:
+
+sudo apt-get install software-properties-common
+
+1. To heck out sevices states
+systemctl list-units --type=service
+
+
+2. Python HTTP Server:
+
+- Servir a pasta atual na porta 8000 (padrÃ£o):
+  python3 -m http.server
+
+- Servir a pasta atual em uma porta especifica (exemplo 4000):
+  python3 -m http.server 4000
+
+- Servir uma pasta especifica:
+  python3 -m http.server 4000 --directory /caminho/para/pasta
+
+- Permitir acesso de outras maquinas na rede (escutar em todas interfaces):
+  python3 -m http.server 4000 --bind 0.0.0.0
+
+
+3. Desabilitar o KVM no Linux
+
+- Verifique se o KVM estao carregado:
+  lsmod | grep kvm
+- Descarregar os modulos do KVM temporariamente
+  sudo rmmod kvm_intel (kvm_amd)
+  sudo rmmod kvm
+
+
+
+4. To install python on ubuntu
+sudo apt-get install python3
+
+sudo apt-get install python3-pip
+
+which python3
+
+python3 --version
+
+pip --version
